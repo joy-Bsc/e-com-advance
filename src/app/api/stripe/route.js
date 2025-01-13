1df5ab2e055a9304bbdc0892673f51246673406a
Joy Dhar <104727780+joy-Bsc@users.noreply.github.com>
2025-01-13 21:58:53 +0600

    Update route.js

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -12,8 +12,8 @@ export async function POST(req){
             payment_method_types: ['card'],
             line_items: res,
             mode: 'payment',
-            success_url: 'http://localhost:3000/checkout' + '?status=success',
-            cancel_url: 'http://localhost:3000/checkout' + '?status=cancel',
+            success_url: 'https://e-com-advance.vercel.app/checkout' + '?status=success',
+            cancel_url: 'https://e-com-advance.vercel.app/checkout' + '?status=cancel',
         })
         return NextResponse.json({
             success: true,
