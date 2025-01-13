6fdd0b80023ca976289b3a082663155c7f566db7
Joy Dhar <104727780+joy-Bsc@users.noreply.github.com>
2025-01-13 22:13:53 +0600

    Update index.js

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -26,7 +26,7 @@ export const getAllCartItems = async(id) =>{
    
     
     try {
-        const res = await fetch(`https://e-com-advance.vercel.app/api/cart/all-cart-items?id=${id}`,{
+        const res = await fetch(`https://shoppingtoday-by-joy.vercel.app/api/cart/all-cart-items?id=${id}`,{
             method:'GET',
             headers:{
                 Authorization: `Bearer ${Cookies.get('token')}`
